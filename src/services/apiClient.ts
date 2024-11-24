bc36760c49f641902034c3719327dd4a68c802ca
Tom <kiyotomm99@gmail.com>
2024-11-25 04:43:45 +0900

    a

---
+++ src/services/apiClient.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// Create an Axios instance
+const apiClient = axios.create({
+  baseURL: "https://api.themoviedb.org/3", // Replace with the actual base URL of the API
+  params: {
+    api_key: "d1ee23eca14e7dec355c6748058fb72f", // Add your client_id or access key
+  },
+});
+
+// Export the Axios instance for reuse
+export default apiClient;
